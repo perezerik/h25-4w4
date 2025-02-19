84bb9a9ed5a2068448731c98cf1575e738971a24
Perez Angel <e2238024@cmaisonneuve.qc.ca>
2025-02-19 17:30:20 -0500

    footer

--- footer.php
+++ footer.php
@@ -1,5 +1,27 @@
 <footer>
-
+  <div class="piedpage global">
+    <section class="piedpage__s1">
+      <div class="piedpage__s1__externe">
+        <?php wp_nav_menu(array(
+          "menu" => "externe",
+          "container" => "nav",
+        )); ?>
+      </div>
+      <div class="piedpage__s1__adresse">
+        <div class="piedpage__s1_adresse__coord">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum commodi molestias debitis nemo, culpa id, magnam similique dolor optio temporibus laborum. Inventore quasi quibusdam dicta rem harum quos amet non?
+        </div>
+        <div class="piedpage__s1_adresse__recherche">
+          <?php get_search_form(); ?>
+        </div>
+      </div>
+      <div class="piedpage__s1__description">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum, nostrum sint deserunt architecto dolorem nisi delectus pariatur odit eius! Maiores dolore provident soluta culpa minus nesciunt doloremque vero incidunt accusamus!
+      </div>
+    </section>
+    <section class="piedpage__s2"></section>
+    <section class="piedpage__s3"></section>
+  </div>
 </footer>
 
 <?php wp_footer() ?>
--- header.php
+++ header.php
@@ -27,40 +27,6 @@
                     'container' => 'nav',
                     'container_class' => 'entete__menu'
                 )); ?>
-                <nav class="entete__menu">
-                    <ul class="menu">
-                        <li class="menu__li">
-                            <a href="#">Aventure</a>
-                        </li>
-                        <li class="menu__li">
-                            <a href="#">Culturel</a>
-                        </li>
-                        <li class="menu__li">
-                            <a href="#">Zen</a>
-                        </li>
-                        <li class="menu__li">
-                            <a href="#">Sportive</a>
-                        </li>
-                        <li class="menu__li">
-                            <a href="#">Croisière</a>
-                        </li>
-                        <li class="menu__li">
-                            <a href="#">Culturel</a>
-                        </li>
-                        <li class="menu__li">
-                            <a href="#">Repos</a>
-                        </li>
-                        <li class="menu__li">
-                            <a href="#">Économique</a>
-                        </li>
-                        <li class="menu__li">
-                            <a href="#">Nature</a>
-                        </li>
-                        <li class="menu__li">
-                            <a href="#">Pays</a>
-                        </li>
-                    </ul>
-                </nav>
                 <form class="recherche">
                     <input type="search" placeholder="Rechercher" class="recherche__input">
                     <img class="recherche__img"  src="https://s2.svgbox.net/hero-outline.svg?ic=search&color=000" width="16" height="16">
