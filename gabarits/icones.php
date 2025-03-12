90caec6078799912e1304186c15d98ec5bf6016b
Perez Angel <e2238024@cmaisonneuve.qc.ca>
2025-03-12 16:18:53 -0400

    s8c2 ajout du fichier icones template

--- gabarits/icones.php
+++ gabarits/icones.php
@@ -3,3 +3,8 @@
     *Template-part icones
 */
 ?>
+<img src="https://s2.svgbox.net/social.svg?ic=facebook&color=000000" width="20" height="20">
+<img src="https://s2.svgbox.net/social.svg?ic=linkedin&color=000000" width="20" height="20">
+<img src="https://s2.svgbox.net/social.svg?ic=wordpress&color=000000" width="20" height="20">
+<img src="https://s2.svgbox.net/social.svg?ic=snapchat&color=000000" width="20" height="20">
+<img src="https://s2.svgbox.net/social.svg?ic=discord&color=000000" width="20" height="20">
