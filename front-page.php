7ceecaf7508c01b47fba72a2db2944d7908bbb04
perezerik <e2238024@cmaisonneuve.qc.ca>
2025-03-17 16:19:42 -0400

    s9c1 quelques scss change

--- front-page.php
+++ front-page.php
@@ -13,27 +13,7 @@
             </div>
     </section>
     <section class="inscription">
-        <form class="inscription__form">
-            <div class="inscription__info">
-                <label for="nom">Nom</label>
-                <input type="text" placeholder="Écrivez votre nom" id="nom" name="nom">
-            </div>
-            <div class="inscription__info">
-                <label for="prenom">Prénom</label>
-                <input type="text" placeholder="Écrivez votre prénom" id="prenom" name="prenom">
-            </div>
-            <div class="inscription__info">
-                <label for="courriel">Courriel</label>
-                <input type="text" placeholder="Écrivez votre courriel" id="courriel" name="courriel">
-            </div>
-            <div class="inscription__info">
-                <label for="telephone">Téléphone</label>
-                <input type="text" placeholder="Écrivez votre téléphone" id="telephone" name="telephone">
-            </div>
-            <div class="inscription__info">
-                <input  class="inscription__boutton" type="submit" value="S'inscrire">
-            </div>
-        </form>
+        <?php  get_template_part('gabarits/inscription'); ?>
     </section>
     <section class="populaire">
         <div class="global">
---
+++ gabarits/inscription.php
@@ -0,0 +1,26 @@
+<?php
+/*
+    *Template-part inscription
+*/
+?>
+<form class="inscription__form">
+        <div class="inscription__info">
+            <label for="nom">Nom</label>
+            <input type="text" placeholder="Écrivez votre nom" id="nom" name="nom">
+        </div>
+        <div class="inscription__info">
+            <label for="prenom">Prénom</label>
+            <input type="text" placeholder="Écrivez votre prénom" id="prenom" name="prenom">
+        </div>
+        <div class="inscription__info">
+            <label for="courriel">Courriel</label>
+            <input type="text" placeholder="Écrivez votre courriel" id="courriel" name="courriel">
+        </div>
+        <div class="inscription__info">
+            <label for="telephone">Téléphone</label>
+            <input type="text" placeholder="Écrivez votre téléphone" id="telephone" name="telephone">
+        </div>
+        <div class="inscription__info">
+            <input  class="inscription__boutton" type="submit" value="S'inscrire">
+        </div>
+</form>
