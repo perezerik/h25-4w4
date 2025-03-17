0a4fc3d76f5cb5230d30fb82bc8f5d4391dadace
perezerik <e2238024@cmaisonneuve.qc.ca>
2025-03-17 18:34:04 -0400

    s9c1 arrangement final du scss

--- category.php
+++ category.php
@@ -1,6 +1,6 @@
 <?php get_header();?>
-<h1><?php  single_cat_title(); ?></h1>
-<p><?php  echo category_description(); ?></p>
+<h1 class="categorie__titre"><?php  single_cat_title(); ?></h1>
+<div class="categorie__description"><?php  echo category_description(); ?></div>
     <section class="populaire">
         <div class="global">
             <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
