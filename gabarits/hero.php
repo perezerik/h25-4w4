ae8ab9d1333175323cb374e4964435e679f0ebbe
perezerik <e2238024@cmaisonneuve.qc.ca>
2025-03-17 15:55:27 -0400

    s9c1 changement dans le gabarit de hero

--- gabarits/hero.php
+++ gabarits/hero.php
@@ -14,5 +14,8 @@
 <p class="hero__adresse">
     5800 Sherbrooke-est Montréal (Québec) H1X 2A2
 </p>
+<p class="hero__telephone">
+    (514) 254-7131
+</p>
 <p class="hero__auteur">Auteur: <?php  echo $hero_auteur ?></p>
 <button class="hero__button">S'inscrire</button>
