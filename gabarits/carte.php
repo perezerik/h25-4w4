f64c1558f5acacb30682a8fae8a7e863d002e58a
perezerik <e2238024@cmaisonneuve.qc.ca>
2025-03-17 17:02:43 -0400

    s9c1 titre de la carte est devenu un lien cliquable

--- gabarits/carte.php
+++ gabarits/carte.php
@@ -10,9 +10,12 @@
       if (has_post_thumbnail()) {
         the_post_thumbnail('thumbnail'); }  
     ?>
-    <h4 class="carte__titre"><?php the_title(); ?></h4>
+    <h4 class="carte__titre">
+      <a href="<?php the_permalink() ?>" class="carte__titre__lien">
+        <?php the_title(); ?>
+      </a>
+    </h4>
     <p class="carte__description"><?php echo wp_trim_words(get_the_content(),10, " ... " ); ?></p>
-    <a class="carte__bouton carte__bouton--actif" href="<?php the_permalink() ?>">suite...</a>
     <?php the_category(); ?>
     <p>Température maximum : <?php the_field('temperature_maximum'); ?> C</p>
   </div>
