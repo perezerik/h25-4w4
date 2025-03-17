6cd457adb0fdd4f2c690607c752c6cecfdcd2dfc
perezerik <e2238024@cmaisonneuve.qc.ca>
2025-03-17 18:22:10 -0400

    s9c1 juste la 2eme categorie saffichent lorsque on clique sur un des menus sur les cartes

--- gabarits/carte.php
+++ gabarits/carte.php
@@ -16,7 +16,22 @@
       </a>
     </h4>
     <p class="carte__description"><?php echo wp_trim_words(get_the_content(),10, " ... " ); ?></p>
-    <?php if(!is_category()) {the_category();} ?>
+    <?php
+      $categories = get_the_category();
+      $categorie_actuelle = get_queried_object();
+
+      if (is_category() && $categorie_actuelle) {
+        echo '<ul class="post-categories">';
+        foreach ($categories as $categorie) {
+          if ($categorie->slug != $categorie_actuelle->slug) {
+            echo '<li><a href="' . get_category_link($categorie->term_id) . '">' . $categorie->name . '</a></li>';
+          }
+        }
+        echo '</ul>';
+      } else {
+        the_category();
+      }
+    ?>
     <p>Température maximum : <?php the_field('temperature_maximum'); ?> C</p>
   </div>
 </article>
