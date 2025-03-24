490055384da6dfd87a788721aebf3f3ba75d0b33
Perez Angel <e2238024@cmaisonneuve.qc.ca>
2025-03-24 14:57:04 -0400

    s9c1 page 404 en gabarit

--- 404.php
+++ 404.php
@@ -3,14 +3,7 @@
     $page_404_message = get_theme_mod('page_404_message', 'Default Title');
     $background_404 = get_theme_mod('background_404', '');
 ?>
-    <div class = "page404" style="background-image: url(<?php echo $background_404 ?>)";>
-        <h1><?php echo $page_404_message; ?></h1>
-        <p>Il semble que le lien que vous avez suivi n'existe pas.</p>
-        
-        <?php  get_search_form() ?>
-
-        <p><a href="<?php echo home_url(); ?>" class="page404__btn">Retour à l'accueil</a></p>
-    </div>
+<?php  get_template_part('gabarits/404'); ?>
 <?php get_footer()?>
 </body>
 </html>
---
+++ gabarits/404.php
@@ -0,0 +1,16 @@
+<?php
+/*
+    *Template-part 404
+*/
+    $page_404_message = get_theme_mod('page_404_message', 'Default Title');
+    $background_404 = get_theme_mod('background_404', '');
+
+?>
+<div class = "page404" style="background-image: url(<?php echo $background_404 ?>)";>
+        <h1><?php echo $page_404_message; ?></h1>
+        <p>Il semble que le lien que vous avez suivi n'existe pas.</p>
+        
+        <?php  get_search_form() ?>
+
+        <p><a href="<?php echo home_url(); ?>" class="page404__btn">Retour à l'accueil</a></p>
+</div>
