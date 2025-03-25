456ca2dbcb92b8f65dd82fb8bd39742c784cb00c
Perez Angel <e2238024@cmaisonneuve.qc.ca>
2025-03-25 14:17:51 -0400

    404 finit

--- 404.php
+++ 404.php
@@ -1,8 +1,4 @@
 <?php get_header();?>
-<?php
-    $page_404_message = get_theme_mod('page_404_message', 'Default Title');
-    $background_404 = get_theme_mod('background_404', '');
-?>
 <?php  get_template_part('gabarits/404'); ?>
 <?php get_footer()?>
 </body>
