1cb6eef11777758fe8eda5650aed4639b1e49dc2
perezerik <e2238024@cmaisonneuve.qc.ca>
2025-05-12 16:17:36 -0400

    s15c1 modification de search pour afficher le nombre de resulats

--- search.php
+++ search.php
@@ -7,6 +7,13 @@ get_header();
 <main class="site__main">
     <section class="recherche__section">
         <?php if (have_posts()) : ?>
+            <h2 class="recherche__titre">
+                <?php
+                    global $wp_query;
+                    $nombre_resultats = $wp_query->found_posts;
+                    echo $nombre_resultats . ' résultat' . ($nombre_resultats > 1 ? 's' : '') . ' trouvé' . ($nombre_resultats > 1 ? 's' : '') . ' pour : "' . get_search_query() . '"';
+                ?>
+            </h2>
             <?php while (have_posts()) : the_post(); ?>
                 <article>
                     <h5><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
@@ -15,7 +22,7 @@ get_header();
                 </article>
             <?php endwhile; ?>
         <?php else : ?>
-            <p>Aucun résultat trouvé.</p>
+            <p>Aucun résultat trouvé pour : "<?php echo get_search_query(); ?>"</p>
         <?php endif; ?>
     </section>
 </main>
