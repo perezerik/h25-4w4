add1848698d4af22f1a7b1fc4e5c3f483164b5f5
perezerik <e2238024@cmaisonneuve.qc.ca>
2025-05-12 16:33:59 -0400

    s15c1 scss pour la page search

--- header.php
+++ header.php
@@ -31,7 +31,9 @@
                     'container' => 'nav',
                     'container_class' => 'entete__menu'
                 )); ?>
-                <?php  get_search_form() ?>
+                <div class="entete__recherche">
+                    <?php get_search_form(); ?>
+                </div>
                 <!-- <form class="recherche">
                     <input type="search" placeholder="Rechercher" class="recherche__input">
                     <img class="recherche__img"  src="https://s2.svgbox.net/hero-outline.svg?ic=search&color=000" width="16" height="16">
