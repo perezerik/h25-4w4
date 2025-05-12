a6d739b481f297821416628af9fd1410202238ba
perezerik <e2238024@cmaisonneuve.qc.ca>
2025-05-12 15:33:09 -0400

    s15c1 ajout scss category

--- category.php
+++ category.php
@@ -1,7 +1,7 @@
 <?php get_header();?>
 <h1 class="categorie__titre"><?php  single_cat_title(); ?></h1>
 <div class="categorie__description"><?php  echo category_description(); ?></div>
-    <section class="populaire">
+    <section class="populaire categorie__cartes">
         <div class="global">
             <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
                 <?php get_template_part('gabarits/carte'); ?>
