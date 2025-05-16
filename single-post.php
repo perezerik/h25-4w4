bc342fcb86c61e823dd38cf75ec0c2a6838f44ff
perezerik <e2238024@cmaisonneuve.qc.ca>
2025-05-16 14:50:15 -0400

    s15c2 single post structure

--- single-post.php
+++ single-post.php
@@ -1,23 +1,34 @@
 <?php get_header();?>
     <section class="populaire">
-        <div class="global">
+        <div class="global single__post">
             <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-            <article>
-            <?php
-                if (has_post_thumbnail()) {
-                the_post_thumbnail('large'); }  
-            ?>
-                <h2><?php the_title(); ?></h2>
-                <div><?php the_content() ?>
-                <?php the_category(); ?>
-                <p>Température maximum : <?php  the_field('temperature_maximum'); ?> C&#176; </p>
-                <p>Température minimum : <?php  the_field('temperature_minimum'); ?> C&#176;</p>
-                <p>Température moyenne : <?php  the_field('temperature_moyenne'); ?> C&#176;</p>
-            </article>
+                <article>
+                    <?php if (has_post_thumbnail()) : ?>
+                        <?php the_post_thumbnail('large'); ?>
+                    <?php else : ?>
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/default.jpg" alt="Image par défaut">
+                    <?php endif; ?>
+                        <h2><?php the_title(); ?></h2>
+
+                        <div class="categories">
+                            <?php the_category(); ?>
+                        </div>
+                        
+
+                        <div class="description">
+                            <div><?php the_content() ?>
+                        </div>
+
+                        <div class="temperatures">
+                            <p>Température maximum : <?php  the_field('temperature_maximum'); ?> C&#176; </p>
+                            <p>Température minimum : <?php  the_field('temperature_minimum'); ?> C&#176;</p>
+                            <p>Température moyenne : <?php  the_field('temperature_moyenne'); ?> C&#176;</p>
+                        </div>
+                </article>
             <?php endwhile; endif; ?>
         </div>
     </section>
-    <?php get_footer()?>
+<?php get_footer()?>
 </body>
 </html>
 
