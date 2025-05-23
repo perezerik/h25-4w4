e5add9622141977e4a931e651bcc43922ef97d82
Perez Angel <e2238024@cmaisonneuve.qc.ca>
2025-05-23 11:40:20 -0400

    scss final et vague fonctionelle

--- template-pays.php
+++ template-pays.php
@@ -15,38 +15,38 @@ $pays = [
 </script>
 
 
-<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-        <article class="template__pays">
-            <h2><?php the_title(); ?></h2>
-            <div><?php the_content(); ?></div>
-        </article>
+        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+            <article class="template__pays">
+                <h2><?php the_title(); ?></h2>
+                <div><?php the_content(); ?></div>
+            </article>
         <?php endwhile; endif; ?>
-<?php creer_vague_avec_gradient("#ff9a9e", "#f97316"); ?>
-<section class="populaire" style="background-color:#f97316" >
-
-    <div class="global" >
-        <div class="evenement">
-            <h2><?php the_field('titre_evenement'); ?></h2>
-            <h2>Date du prochain événement</h2>
-            <p><?php the_field('date_evenement'); ?></p>
-            <h2>Coordonnées de l'évènement</h2>
-            <p><?php the_field('coordonne_evenement'); ?></p>
-        </div>
-
-        <!-- ////////////////////////////////////////////////  section rest-api -->
         
-        <section class="destination">
-            <h2 class="destination__titre pays__nom">Pays</h2>
-            <div class="menu-pays">
-            <?php foreach ($pays as $nom): ?>
-                <button class="menu-pays__bouton" data-search="<?= esc_attr($nom) ?>">
-                <?= esc_html($nom) ?>
-                </button>
-            <?php endforeach; ?>
+            <div class="evenement">
+                    <h2><?php the_field('titre_evenement'); ?></h2>
+                    <h2>Date du prochain événement</h2>
+                    <p><?php the_field('date_evenement'); ?></p>
+                    <h2>Coordonnées de l'évènement</h2>
+                    <p><?php the_field('coordonne_evenement'); ?></p>
             </div>
-            <div class="destination__list"></div>
-    </section>
+            <?php creer_vague_avec_gradient("#ff9a9e", "#f97316"); ?>
+            <div style="background-color:#f97316;">
+            <!-- ////////////////////////////////////////////////  section rest-api -->
+            
+            <section class="destination">
+                
+                    <h2 class="destination__titre pays__nom">Pays</h2>
+                    <div class="menu-pays">
+                    <?php foreach ($pays as $nom): ?>
+                        <button class="menu-pays__bouton" data-search="<?= esc_attr($nom) ?>">
+                        <?= esc_html($nom) ?>
+                        </button>
+                    <?php endforeach; ?>
+                    </div>
+                    <div class="destination__list"></div>
+            </section>
 
-    </div>
-</section>
-<?php get_footer(); ?>
+            <?php creer_vague_avec_gradient("#ff9a9e", "#f97316"); ?>
+            <?php get_footer(); ?>
+        </div>
+        
